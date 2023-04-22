66c188166892ee11ee46034335468ca42b92406d
IndraBlue <102951217+IndraBlue@users.noreply.github.com>
2023-04-22 21:35:24 +0530

    Create transfer.py
    transfer of money from one account to another account

---
+++ transfer.py
@@ -0,0 +1,6 @@
+Sender_ACC=input("Enter the sender account no.: ")
+Reciever_ACC=input("Enter the reciever account no.: ")
+amt=int(input("Amount to be transfered: "))
+
+for j in a:
+  j.Transfer_Amt(Sender_ACC,Reciever_ACC,amt)
